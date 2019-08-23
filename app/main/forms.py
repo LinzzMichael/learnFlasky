9862cb43e77d1eedec5e0915b3c607c94f37796f
林志强 <linzhiqiang@Lin-4.local>
2019-08-23 23:22:35 +0800

    10d

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm, Form
-from wtforms import StringField, SubmitField,TextAreaField
-from wtforms.validators import DataRequired,Required, Length
+from wtforms import StringField, SubmitField,TextAreaField,BooleanField, SelectField
+from wtforms.validators import DataRequired,Required, Length, Email, Regexp
+from ..models import Role, User
 
 
 class NameForm(FlaskForm):
@@ -14,3 +15,31 @@ class EditProfileForm(Form):
 	location = StringField('location', validators=[Length(0,64)])
 	about_me = TextAreaField('about_me')
 	submit = SubmitField('Submit')
+
+
+
+
+class EditProfileAdminForm(Form):
+	email = StringField('Email', validators=[Required(), Length(1,64), Email()])
+	username = StringField('username', validators=[Required(), Length(1,64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers, dots or undersocres')])
+	confirmed = BooleanField('Confirmed')
+	role = SelectField('Role', coerce=int)
+	name = StringField('Real name', validators=[Length(1,64)])
+	loacation = StringField('Location', validators=[Length(1,64)])
+	about_me = TextAreaField('About_me')
+	submit = SubmitField('Submit')
+
+	def __init__(self, user, *args, **kwargs):
+		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+		self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+		self.user = user
+
+	def vaildate_email(self, field):
+		if field.data != self.user.email and \
+				User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
+
+	def vaildate_username(self, field):
+		if field.data != sefl.user.username and \
+				User.query.filter_by(username=field.data).first():
+			raise VaildationError('username already in use.')
--- app/models.py
+++ app/models.py
@@ -4,8 +4,9 @@ from . import login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from . import db
-from flask import current_app
+from flask import current_app, request
 from datetime import datetime
+import hashlib
 
 
 
@@ -67,16 +68,22 @@ class User(UserMixin,db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
-
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         if self.role is None:
             if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
 
+
+    #更改邮件之后重新计算邮箱的哈希值存在数据库中，用于下次获取用户头像
+    # def change_email(self, token):
+    #     self.email = new_email
 
     def can(self, permissions):
         return self.role is not None and \
@@ -127,6 +134,15 @@ class User(UserMixin,db.Model):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
 
+    #用户头像函数
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'http://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
+
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
@@ -135,4 +151,7 @@ class AnonymousUser(AnonymousUserMixin):
     def is_adminstrator(self):
         return False
 login_manager.anonymous_user = AnonymousUser
+
+
+
 
